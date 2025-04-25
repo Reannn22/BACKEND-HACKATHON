d44a9c347a0f3895a7ab22223408fa5f88ce7483
reannn22 <reyhan.123140022@student.itera.ac.id>
2025-04-25 16:14:05 +0700

    automate push

--- database/migrations/2024_01_16_000000_create_items_table.php
+++ database/migrations/2024_01_16_000000_create_items_table.php
@@ -8,13 +8,27 @@ return new class extends Migration
 {
     public function up()
     {
-        Schema::create('items', function (Blueprint $table) {
-            $table->id();
-            $table->string('nama_barang');
-            $table->string('kode_barang');
-            $table->string('merk_barang');
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('items')) {
+            Schema::create('items', function (Blueprint $table) {
+                $table->id();
+                $table->string('nama_barang');
+                $table->string('kode_barang');
+                $table->string('merk_barang');
+                $table->timestamps();
+            });
+        } else {
+            Schema::table('items', function (Blueprint $table) {
+                if (!Schema::hasColumn('items', 'nama_barang')) {
+                    $table->string('nama_barang');
+                }
+                if (!Schema::hasColumn('items', 'kode_barang')) {
+                    $table->string('kode_barang');
+                }
+                if (!Schema::hasColumn('items', 'merk_barang')) {
+                    $table->string('merk_barang');
+                }
+            });
+        }
     }
 
     public function down()
